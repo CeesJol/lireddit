51859792867493b3889d39bb159ce286146984b4
Cees Jol <cjol99@icloud.com>
2021-08-24 10:45:10 +0200

    🎨 Remove home page code duplication

---
+++ web/src/components/Home.tsx
@@ -0,0 +1,89 @@
+import { Button, Flex, Select, Stack } from "@chakra-ui/react";
+import React from "react";
+import { Layout } from "../components/Layout";
+import PostComponent from "../components/PostComponent";
+import { usePostsQuery } from "../generated/graphql";
+import { changeSort } from "../util/changeSort";
+
+interface HomeProps {
+  sort: "top" | "new";
+}
+
+interface SortSpecificVariablesOptions {
+  offset?: number;
+  cursor?: string;
+}
+
+const Home: React.FC<HomeProps> = ({ sort }) => {
+  let { data, error, loading, fetchMore, variables } = usePostsQuery({
+    variables: {
+      limit: 15,
+      sort: sort,
+      cursor: null,
+    },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  if (!loading && !data) {
+    return (
+      <div>
+        <div>you got query failed for some reason</div>
+        <div>{error?.message}</div>
+      </div>
+    );
+  }
+
+  return (
+    <Layout>
+      Sort by:
+      <Select
+        placeholder="Select option"
+        w={100}
+        onChange={(event) => changeSort(event)}
+        value={sort}
+        mb={4}
+      >
+        <option value="new">New</option>
+        <option value="top">Top</option>
+      </Select>
+      {!data && loading ? (
+        <div>loading...</div>
+      ) : (
+        <Stack spacing={8}>
+          {data!.posts.posts.map((p) =>
+            !p ? null : <PostComponent p={p} key={p.id} />
+          )}
+        </Stack>
+      )}
+      {data && data.posts.hasMore ? (
+        <Flex>
+          <Button
+            onClick={() => {
+              let sortSpecificVariables: SortSpecificVariablesOptions = {};
+              if (sort === "top") {
+                sortSpecificVariables.offset = data!.posts.offset + 15;
+              } else {
+                sortSpecificVariables.cursor =
+                  data!.posts.posts[data!.posts.posts.length - 1].createdAt;
+              }
+              fetchMore({
+                variables: {
+                  limit: variables?.limit,
+                  sort: sort,
+                  ...sortSpecificVariables,
+                },
+              });
+            }}
+            isLoading={loading}
+            m="auto"
+            my={8}
+          >
+            load more
+          </Button>
+        </Flex>
+      ) : null}
+    </Layout>
+  );
+};
+
+export default Home;
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,75 +1,9 @@
-import { Button, Flex, Select, Stack } from "@chakra-ui/react";
 import React from "react";
-import { Layout } from "../components/Layout";
-import PostComponent from "../components/PostComponent";
-import { usePostsQuery } from "../generated/graphql";
-import { changeSort } from "../util/changeSort";
+import Home from "../components/Home";
 import { withApollo } from "../util/withApollo";
 
 const Index = () => {
-  let { data, error, loading, fetchMore, variables } = usePostsQuery({
-    variables: {
-      limit: 15,
-      sort: "new",
-      cursor: null,
-    },
-    notifyOnNetworkStatusChange: true,
-  });
-
-  if (!loading && !data) {
-    return (
-      <div>
-        <div>you got query failed for some reason</div>
-        <div>{error?.message}</div>
-      </div>
-    );
-  }
-
-  return (
-    <Layout>
-      Sort by:
-      <Select
-        placeholder="Select option"
-        w={100}
-        onChange={(event) => changeSort(event)}
-        value="new"
-        mb={4}
-      >
-        <option value="new">New</option>
-        <option value="top">Top</option>
-      </Select>
-      {!data && loading ? (
-        <div>loading...</div>
-      ) : (
-        <Stack spacing={8}>
-          {data!.posts.posts.map((p) =>
-            !p ? null : <PostComponent p={p} key={p.id} />
-          )}
-        </Stack>
-      )}
-      {data && data.posts.hasMore ? (
-        <Flex>
-          <Button
-            onClick={() => {
-              fetchMore({
-                variables: {
-                  limit: variables?.limit,
-                  sort: "new",
-                  cursor:
-                    data!.posts.posts[data!.posts.posts.length - 1].createdAt,
-                },
-              });
-            }}
-            isLoading={loading}
-            m="auto"
-            my={8}
-          >
-            load more
-          </Button>
-        </Flex>
-      ) : null}
-    </Layout>
-  );
+  return <Home sort="new" />;
 };
 
 export default withApollo({ ssr: true })(Index);
